e6d1f70da5d0cba8cf644f6e07de1c4c84b2b585
Cooper Wolfden <43880675+cooperwo1f@users.noreply.github.com>
2024-06-20 23:57:20 +0930

    removed final references, added comments

--- Core/Src/user.c
+++ Core/Src/user.c
@@ -5,30 +5,33 @@
 #include <stdio.h>
 #include <string.h>
 
+/* BASE ADDR OF CONTROLLER */
 #define BASE_ADDR 0
 #include "led.h"
 
+/* TIMER HANDLERS */
 extern TIM_HandleTypeDef htim2;
 extern TIM_HandleTypeDef htim3;
-extern TIM_HandleTypeDef htim4;
 
+/* DMA HANDLERS */
 extern DMA_HandleTypeDef hdma_tim2_up;
 extern DMA_HandleTypeDef hdma_tim3_up;
-extern DMA_HandleTypeDef hdma_tim4_up;
 
+/* ARRAY CONTAINING LED CHANNEL VALUES */
 #define CHANNELS_LENGTH 256
-
 uint16_t channels[CHANNELS_LENGTH];
 
+/* VALUES TO BE CONTINUALLY TRANSFERRED VIA DMA */
 uint32_t data_A[256];
 uint32_t data_B[256];
 
 void init(void) {
+	/* CLEAR ALL ARRAYS */
 	memset(&channels, 0, sizeof(channels));
-
 	memset(&data_A, 0, sizeof(data_A));
 	memset(&data_B, 0, sizeof(data_B));
 
+	/* START TIMERS */
 	HAL_TIM_Base_Start(&htim2);
 	HAL_TIM_Base_Start(&htim3);
 
@@ -40,15 +43,18 @@ void init(void) {
 	__HAL_TIM_ENABLE_DMA(&htim2, TIM_DMA_UPDATE);
 	__HAL_TIM_ENABLE_DMA(&htim3, TIM_DMA_UPDATE);
 
+	/* SIGNAL PROGRAM RUNNING */
 	HAL_GPIO_WritePin(LED_GPIO_Port, LED_Pin, GPIO_PIN_SET);
 }
 
 void run(void) {
+	/* WRITE PWM VALUES FROM CHANNEL ARRAY */
 	for (size_t i = 0; i < CHANNELS_LENGTH; i++) {
 		led_write(data_A, data_B, sizeof(data_A)/sizeof(data_A[0]), i, channels[i]);
 	}
 }
 
 void err(void) {
+	/* CLEAR PROGRAM RUNNING SIGNAL */
 	HAL_GPIO_WritePin(LED_GPIO_Port, LED_Pin, GPIO_PIN_RESET);
 }
